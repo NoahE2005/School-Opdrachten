06b8d4b394855c70d1caca9d5668c3d0ab9b08be
NoahE2005 <9014121@student.zadkine.nl>
2022-10-12 12:55:16 +0200

    e

---
+++ Java Opdracht 5.1/Script.js
@@ -0,0 +1,15 @@
+
+  function Alert() {
+    alert ("Success");
+    var color2 = prompt ("Color?");
+    document.getElementById("Java").style.color = color2;
+    var color3 = prompt ("Color?");
+    document.getElementById("Java2").style.color = color2;
+  }
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+
+
+
