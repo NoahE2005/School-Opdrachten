e12636cb800988825a966d836cccded244cb219f
NoahE2005 <9014121@student.zadkine.nl>
2022-10-12 13:26:10 +0200

    d

--- Java Opdracht 6.1/Script.js
+++ Java Opdracht 6.1/Script.js
@@ -1,11 +1,38 @@
 
   function Alert() {
     alert ("Success");
-    var color2 = prompt ("Color?");
+    var color2 = prompt ("Color text1?");
     document.getElementById("Java").style.color = color2;
-    var color3 = prompt ("Color?");
+    var color3 = prompt ("Color text2?");
     document.getElementById("Java2").style.color = color3;
+    var color4 = prompt ("Color background?");
+    document.body.style.backgroundColor = color4;
   }
+
+  function Disco() {
+    document.body.style.backgroundColor = "red";
+    setTimeout(Disco2, 500)
+  } 
+  function Disco2() {
+    document.body.style.backgroundColor = "orange";
+    setTimeout(Disco3, 500)
+  }
+  function Disco3() {
+    document.body.style.backgroundColor = "yellow";
+    setTimeout(Disco4, 500)
+  }
+  function Disco4() {
+    document.body.style.backgroundColor = "green";
+    setTimeout(Disco5, 500)
+  }
+  function Disco5() {
+    document.body.style.backgroundColor = "white";
+  }
+
+
+
+
+
 
 var getal1 = 12;
 var getal2 = 4;
