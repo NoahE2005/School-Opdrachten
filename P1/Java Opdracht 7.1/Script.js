1deba02315c549dc4a68c6efa5e7ab2d4962c673
NoahE2005 <9014121@student.zadkine.nl>
2022-10-17 15:00:25 +0200

    d

--- P1/Java Opdracht 7.1/Script.js
+++ P1/Java Opdracht 7.1/Script.js
@@ -1,23 +1,36 @@
 var getal = 0
 
-function BudgetCheck() {
+function BudgetCheck72() {
     var Budget = prompt ("Amount?");
     var MinBudget = 100;
 
     if (Budget>MinBudget) {
-        alert(Valid);
         document.getElementById("Text-Budget").style.color = "green";
+        document.getElementById("Text-Budget"). textContent  = "Meer dan 100";
     }
     else {
-        alert(Not_Valid);
         document.getElementById("Text-Budget").style.color = "red";
+        document.getElementById("Text-Budget"). textContent  = "Minder dan 100";
     }
-    alert("Sucess")
 }
 function Tekst73() {
     document.getElementById("7.3Id"). textContent = getal++;
     if(getal>10) 
     {
         getal = 0
+    }
+}
+
+function BudgetCheck71() {
+    var Budget2 = 3;
+    var MinBudget2 = 100;
+
+    if (Budget2>MinBudget2) {
+        document.getElementById("Text-Budget2").style.color = "green";
+        document.getElementById("Text-Budget2"). textContent  = "Meer dan 100";
+    }
+    else {
+        document.getElementById("Text-Budget2").style.color = "red";
+        document.getElementById("Text-Budget2"). textContent  = "Minder dan 100";
     }
 }
