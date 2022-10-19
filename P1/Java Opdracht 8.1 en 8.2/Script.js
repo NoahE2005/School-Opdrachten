aca5115d6cc6c1d3df45d7c9c42314968ca04e2a
NoahE2005 <9014121@student.zadkine.nl>
2022-10-19 12:54:18 +0200

    a

--- P1/Java Opdracht 8.1 en 8.2/Script.js
+++ P1/Java Opdracht 8.1 en 8.2/Script.js
@@ -10,3 +10,42 @@ function Plus() {
 
     }
 }
+
+function Minus() {
+    var field1 = document.getElementById('num1').value;
+    var field2 = document.getElementById('num2').value;
+
+    var result = parseFloat(field1) - parseFloat(field2);
+    if (!isNaN(result))
+
+    {
+        document.getElementById("TextChange").textContent= "Het antwoord is " + result;
+
+    }
+}
+
+function Multiply() {
+    var field1 = document.getElementById('num1').value;
+    var field2 = document.getElementById('num2').value;
+
+    var result = parseFloat(field1) * parseFloat(field2);
+    if (!isNaN(result))
+
+    {
+        document.getElementById("TextChange").textContent= "Het antwoord is " + result;
+
+    }
+}
+
+function Divide() {
+    var field1 = document.getElementById('num1').value;
+    var field2 = document.getElementById('num2').value;
+
+    var result = parseFloat(field1) / parseFloat(field2);
+    if (!isNaN(result))
+
+    {
+        document.getElementById("TextChange").textContent= "Het antwoord is " + result;
+
+    }
+}
