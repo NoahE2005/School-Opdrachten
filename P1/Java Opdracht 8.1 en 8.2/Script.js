5166f504d63743ac678a310f5827caba22f82403
NoahE2005 <9014121@student.zadkine.nl>
2022-10-19 11:26:20 +0200

    a

---
+++ P1/Java Opdracht 8.1 en 8.2/Script.js
@@ -0,0 +1,12 @@
+function Plus() {
+    var field1 = document.getElementById('num1').value;
+    var field2 = document.getElementById('num2').value;
+
+    var result = parseFloat(field1) + parseFloat(field2);
+    if (!isNaN(result))
+
+    {
+        document.getElementById("TextChange").textContent= "Het antwoord is " + result;
+
+    }
+}
