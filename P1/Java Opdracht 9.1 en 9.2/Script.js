fde13585e012c28b8f52063670e7099e1faf05ad
NoahE2005 <9014121@student.zadkine.nl>
2022-10-21 12:33:06 +0200

    a

--- P1/Java Opdracht 9.1 en 9.2/Script.js
+++ P1/Java Opdracht 9.1 en 9.2/Script.js
@@ -8,3 +8,39 @@ function ChangeImg() {
    }
 
 }
+
+function ChangeImg2() {
+        var x = document.getElementById("Monkey2");
+        if (x.innerHTML === "🙉") {
+          x.innerHTML = "🐵";
+        } else {
+          x.innerHTML = "🙉";
+        }
+      }
+
+      
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
