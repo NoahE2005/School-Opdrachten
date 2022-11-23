e58b9bc84b6c1bd00ce1867c329006ec84dac31f
NoahE2005 <9014121@student.zadkine.nl>
2022-11-23 09:38:50 +0100

    Update Kleuren.js

--- P2/Javascript Oefenen/Oefening1/Kleuren.js
+++ P2/Javascript Oefenen/Oefening1/Kleuren.js
@@ -9,7 +9,7 @@ function kleuraanpassen() {
     const AlleP = document.getElementsByTagName("P")
 
     for (let i = 0; i < AlleP.length; i++) { // Een loop die begint op 0 gaat tot de 4
-    var randomColor = Math.floor(Math.random()*16755556).toString(16); // een willekerige kleuren code pakken (Hex color)
+    var randomColor = Math.floor(Math.random()*26777215).toString(16); // een willekerige kleuren code pakken (Hex color)
   document.getElementsByTagName("p")[i].style.color = "#" + randomColor; //de kleur veranderen door de "RandomColor" variabele
 }
 }
