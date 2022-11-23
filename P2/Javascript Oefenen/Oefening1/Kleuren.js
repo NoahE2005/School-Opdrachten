43019816b59e1bb5a7135229df8d30e393ee89f0
NoahE2005 <9014121@student.zadkine.nl>
2022-11-23 09:26:36 +0100

    Update Kleuren.js

--- P2/Javascript Oefenen/Oefening1/Kleuren.js
+++ P2/Javascript Oefenen/Oefening1/Kleuren.js
@@ -5,7 +5,7 @@ function kleuraanpassen() {
     getal++; //getal +1
     document.getElementById("teller").textContent = "het getal is " + getal; //de tekst als getal zetten
 
-    
+
     const AlleP = document.getElementsByTagName("P")
 
     for (let i = 0; i < AlleP.length; i++) { // Een loop die begint op 0 gaat tot de 4
