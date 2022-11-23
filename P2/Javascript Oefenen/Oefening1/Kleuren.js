d1ae66f182c1379a2f284366d5d318fb89defb9f
NoahE2005 <9014121@student.zadkine.nl>
2022-11-23 09:06:27 +0100

    Update Kleuren.js

--- P2/Javascript Oefenen/Oefening1/Kleuren.js
+++ P2/Javascript Oefenen/Oefening1/Kleuren.js
@@ -2,12 +2,11 @@ var getal = 0
 var randomColor = 0
 
 function kleuraanpassen() {
-    getal++;
-    document.getElementById("teller").textContent = "het getal is " + getal;
+    getal++; //getal +1
+    document.getElementById("teller").textContent = "het getal is " + getal; //de tekst als getal zetten
 
-
-    for (let i = 0; i < 4; i++) {
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
-  document.getElementsByTagName("p")[i].style.color = "#" + randomColor;
+    for (let i = 0; i < 4; i++) { // Een loop die begint op 0 gaat tot de 4
+    var randomColor = Math.floor(Math.random()*16777215).toString(16); // een willekerige kleuren code pakken (Hex color)
+  document.getElementsByTagName("p")[i].style.color = "#" + randomColor; //de kleur veranderen door de "RandomColor" variabele
 }
 }
