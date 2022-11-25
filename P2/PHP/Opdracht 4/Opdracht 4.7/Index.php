9aa891b27d472312ae8c126d51fb834383ee034c
NoahE2005 <9014121@student.zadkine.nl>
2022-11-25 12:50:25 +0100

    Update Index.php

--- P2/PHP/Opdracht 4/Opdracht 4.7/Index.php
+++ P2/PHP/Opdracht 4/Opdracht 4.7/Index.php
@@ -5,10 +5,10 @@ if ($spaargeld > 1000) {
     echo "U kan de telefoon kopen";
 }
 elseif ($spaargeld > 750) {
-    echo "U heeft de telefoon bijna, u moet nog" + $spaargeld + " euro verdienen";
+    echo "U heeft de telefoon bijna, u moet nog" .$spaargeld. " euro verdienen";
 }
 elseif ($spaargeld < 750) {
-    echo "U moet een baantje krijgen, u moet nog" + $spaargeld + " euro verdienen";
+    echo "U moet een baantje krijgen, u moet nog" .$spaargeld. " euro verdienen";
 }
 
 
