e5fea9a7c94508aa1e2816c381143e311a0031b9
NoahE2005 <9014121@student.zadkine.nl>
2022-11-25 12:12:40 +0100

    a

--- P2/PHP/Opdracht 4/Opdracht 4.6/Index.php
+++ P2/PHP/Opdracht 4/Opdracht 4.6/Index.php
@@ -1,13 +1,13 @@
 <?php 
 $tijd = Date('H');
-$temperatuur = 20;
-$luchtvochtigheid = 60;
+$temperatuur = 25;
+$luchtvochtigheid = 45;
 
-if ($tijd <17 || $temperatuur < 20 || $luchtvochtigheid < 85) {
-    echo "De airco is aan";
+if ($tijd > 17 || $temperatuur > 20 && $luchtvochtigheid > 85) {
+    echo "De airco is uit";
 }
 else {
-    echo "De airco is uit";
+    echo "De airco is aan";
 }
 
 
---
+++ P2/PHP/Opdracht 4/Opdracht 4.7/Index.php
@@ -0,0 +1,15 @@
+<?php 
+$spaargeld = 600;
+
+if ($spaargeld > 1000) {
+    echo "U kan de telefoon kopen";
+}
+elseif ($spaargeld > 750) {
+    echo "U heeft de telefoon bijna, u moet nog" + $spaargeld + " euro verdienen";
+}
+elseif ($spaargeld < 750) {
+    echo "U moet een baantje krijgen, u moet nog" + $spaargeld + " euro verdienen";
+}
+
+
+?>
