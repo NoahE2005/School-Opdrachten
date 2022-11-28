160b34ec16e56b669859791f7a6bf225b2c37ce9
NoahE2005 <9014121@student.zadkine.nl>
2022-11-28 13:04:37 +0100

    Update Index.php

--- P2/PHP/Opdracht 4/Opdracht 4.6/Index.php
+++ P2/PHP/Opdracht 4/Opdracht 4.6/Index.php
@@ -3,7 +3,7 @@ $tijd = Date('H');
 $temperatuur = 25;
 $luchtvochtigheid = 45;
 
-if ($tijd > 17 || $temperatuur > 20 && $luchtvochtigheid > 85) {
+if ($tijd > 17 || $temperatuur < 20 && $luchtvochtigheid < 85) {
     echo "De airco is uit";
 }
 else {
