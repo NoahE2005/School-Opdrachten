5e66758be9e5254cbc46337eb71c0ccdb71fa506
NoahE2005 <9014121@student.zadkine.nl>
2022-12-02 13:54:36 +0100

    Update Index.php

--- P2/PHP/Opdracht 7/Opdracht 7.1/Index.php
+++ P2/PHP/Opdracht 7/Opdracht 7.1/Index.php
@@ -1,22 +1,30 @@
 
-
-
 <form method="post">
-    <input type="text" name="tekstvak">
-    <input type="radio" id="9procent">
-    <input type="radio" id="21procent">
+    <input type="text" name="tekstvak"></input>
+    <input type="radio" id="9procent" name="Nprocent" value="0">9% Btw</input>
+    <input type="radio" id="21procent" name="Tprocent" value="1">21% Btw</input>
     <br>
-    <input type="button" id="berekenen">
+    <input type="button" id="berekenen" onclick="BtwCheck()">BTW berekenen</input>
 </form>
 
 <?php 
-echo "Btw";
-$check['tekstvak'];
-$Negencheck["9procent"];
-$TwintigCheck["21procent"];
+$BTWhoog = true;
 
+if(isset($_GET['Nprocent'])){
+    $BTWhoog = false;
+    } else {
+    $BTWhoog = true;
+    }
+
+echo "<br><br><br>";
 function BtwCheck() {
+    if($BTWhoog){
+        echo "9procent";
+        } 
     
+    else {
+        echo "21procent";
+        }
 }
 
 ?>
