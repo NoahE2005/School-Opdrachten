c7afe97f98d7f9fc7060bfca7961560e5a92f07c
NoahE2005 <9014121@student.zadkine.nl>
2022-12-02 13:12:44 +0100

    Create Index.php

---
+++ P2/PHP/Opdracht 7/Opdracht 7.1/Index.php
@@ -0,0 +1,22 @@
+
+
+
+<form method="post">
+    <input type="text" name="tekstvak">
+    <input type="radio" id="9procent">
+    <input type="radio" id="21procent">
+    <br>
+    <input type="button" id="berekenen">
+</form>
+
+<?php 
+echo "Btw";
+$check['tekstvak'];
+$Negencheck["9procent"];
+$TwintigCheck["21procent"];
+
+function BtwCheck() {
+    
+}
+
+?>
