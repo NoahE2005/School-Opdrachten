916c471420a7d317d2b5b3bb02c15ae21e625924
NoahE2005 <9014121@student.zadkine.nl>
2022-12-09 12:32:44 +0100

    Update Index.php

---
+++ P2/PHP/Opdracht 7/Opdracht 7.3/Index.php
@@ -0,0 +1,18 @@
+<html>
+    <form>
+    <input type="radio" name="btw" value="  --primary-color: yellow;">laag tarief - 6%<br>
+    <input type="radio" name="btw" value="  --primary-color: red;" checked>hoog tarief - 19%<br>
+    <input type="submit" value="Bereken BTW">
+    </form>
+</html>
+
+<style>
+    body {
+        background-color: var(--Color, white);
+    }
+</style>
+<?php 
+
+
+
+?>
