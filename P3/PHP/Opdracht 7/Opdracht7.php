1b9df7dbfb41056838a646cd3618dcd956ef7405
NoahE2005 <9014121@student.zadkine.nl>
2023-02-24 13:31:16 +0100

    Update Opdracht7.php

--- P3/PHP/Opdracht 7/Opdracht7.php
+++ P3/PHP/Opdracht 7/Opdracht7.php
@@ -34,29 +34,40 @@
 	</form>
 
 	<?php
-    $name = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "schoool";
+		$name = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "schoool";
 		$conn = mysqli_connect($name, $username, $password, $dbname);
 
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$choice = $_POST["choice"];
 
-			$sql = "INSERT INTO poll (choice) VALUES ('$choice')";
+			if ($choice == "1") {
+				$sql = "UPDATE poll SET stem1 = stem1 + 1";
+			} elseif ($choice == "2") {
+				$sql = "UPDATE poll SET stem2 = stem2 + 1";
+			} elseif ($choice == "3") {
+				$sql = "UPDATE poll SET stem3 = stem3 + 1";
+			} elseif ($choice == "4") {
+				$sql = "UPDATE poll SET stem4 = stem4 + 1";
+			}
+
 			mysqli_query($conn, $sql);
 		}
 
-		$sql = "SELECT choice, COUNT(*) AS count FROM poll GROUP BY choice";
+		$sql = "SELECT stem1, stem2, stem3, stem4 FROM poll";
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) > 0) {
 			echo "<h2>Poll Results</h2>";
 			echo "<table>";
 			echo "<tr><th>Choice</th><th>Count</th></tr>";
-			while($row = mysqli_fetch_assoc($result)) {
-				echo "<tr><td>" . $row["choice"] . "</td><td>" . $row["count"] . "</td></tr>";
-			}
+			$row = mysqli_fetch_assoc($result);
+			echo "<tr><td>1</td><td>" . $row["stem1"] . "</td></tr>";
+			echo "<tr><td>2</td><td>" . $row["stem2"] . "</td></tr>";
+			echo "<tr><td>3</td><td>" . $row["stem3"] . "</td></tr>";
+			echo "<tr><td>4</td><td>" . $row["stem4"] . "</td></tr>";
 			echo "</table>";
 		} else {
 			echo "No results found.";
